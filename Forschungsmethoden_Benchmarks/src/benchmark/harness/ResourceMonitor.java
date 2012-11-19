0b3e5aa77827943d5de0136ae96f8c849e85003d
fleckb <bernhard.fleck@gmail.com>
2012-11-19 22:10:21 +0100

    implemented basic resource monitoring

--- Forschungsmethoden_Benchmarks/src/benchmark/harness/ResourceMonitor.java
+++ Forschungsmethoden_Benchmarks/src/benchmark/harness/ResourceMonitor.java
@@ -1,70 +1,60 @@
 package benchmark.harness;
 
 import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
-import java.lang.management.MemoryUsage;
+import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.ThreadMXBean;
-import java.util.HashMap;
-
-import benchmark.reporter.CsvReporter;
-import benchmark.reporter.Reporter;
-
 
 /**
  * Monitors the resource usage during a benchmark run.
  * 
  * @author fleckb
- *
  */
-public class ResourceMonitor implements Runnable{
+public class ResourceMonitor {
 	
-	//Bean for CPU Benchmarks
-	ThreadMXBean threadBean = null;
-	MemoryMXBean memoryBean = null;
+	private final StopWatch watch = new DefaultSystemStopWatch();
 	
-	Reporter reporter = null;
-	
-	private volatile boolean stop;
-	private int intervall = 0;
-	
-	public ResourceMonitor(){}
-	
-	public ResourceMonitor(String string, int intervall){
-
-		memoryBean =  ManagementFactory.getMemoryMXBean();
+	//Bean for CPU and memory benchmarks
+	private final ThreadMXBean threadBean;
+	//private final MemoryMXBean memoryBean;
+		
+	private long startCpuTime;
+	private long stopCpuTime;		
+	//private volatile boolean stop;
+		
+	/**
+	 * TODO:
+	 * @param sampleInterval Sample interval in milliseconds.
+	 * Zero means no sampling: Just take the average in the end.
+	 */
+	public ResourceMonitor(){
 		threadBean = ManagementFactory.getThreadMXBean();
-		
-		reporter = new CsvReporter(string+"CpuMem");
-		this.intervall = intervall;
-		
 	}
-
-	public void stop(){
-		this.stop = true;
+	
+	public void start() {
+		for(MemoryPoolMXBean memoryPool : ManagementFactory.getMemoryPoolMXBeans()) {
+			memoryPool.resetPeakUsage();
+		}		
+		startCpuTime = threadBean.getCurrentThreadCpuTime();
+		watch.start();
 	}
+	
+	public ResourceUsage stop() {
+		stopCpuTime = threadBean.getCurrentThreadCpuTime();
+		long duration = watch.stop();
+		long cpuTime = stopCpuTime - startCpuTime;
 		
-	@SuppressWarnings("static-access")
-	@Override
-	public void run() {
-		while(!stop){
-			logValues(System.currentTimeMillis(),
-					threadBean.getCurrentThreadCpuTime(),
-					memoryBean.getHeapMemoryUsage().getUsed());
-			try {
-				Thread.currentThread().sleep(intervall);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}		
+		float peakMemoryUsed = 0;
+		for(MemoryPoolMXBean memoryPool : ManagementFactory.getMemoryPoolMXBeans()) {
+			peakMemoryUsed += byteToMb(memoryPool.getPeakUsage().getUsed());
 		}
-			
-	}
 		
-	public void logValues(long time, long cpu, long mem){
-		/*
-		 * TODO
-		reporter.report(new String(String.valueOf(time)+";"+
-						String.valueOf(cpu)+";"+
-						String.valueOf(mem)+";"));
-						*/
+		ResourceUsage usage = new ResourceUsage();
+		usage.averageCpuUsage = (float)cpuTime / (float)duration;
+		usage.peakMemoryUsed = peakMemoryUsed;
+		return usage;
+	}
+	
+	private float byteToMb(long bytes) {
+		return (float)bytes / (float)(1024 * 1024);
 	}
 }
