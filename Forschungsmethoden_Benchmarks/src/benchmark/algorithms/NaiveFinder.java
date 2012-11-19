6cf40670d7bfa56b050431cc339ace978b30f604
fleckb <bernhard.fleck@gmail.com>
2012-11-19 22:07:27 +0100

    finalized naive search

--- Forschungsmethoden_Benchmarks/src/benchmark/algorithms/NaiveFinder.java
+++ Forschungsmethoden_Benchmarks/src/benchmark/algorithms/NaiveFinder.java
@@ -1,8 +1,10 @@
 package benchmark.algorithms;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-
-import benchmark.utility.StreamsHelper;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Naive search implementation.
@@ -13,17 +15,46 @@ import benchmark.utility.StreamsHelper;
  */
 public class NaiveFinder implements Finder {
 	
-	FinderStatusListener statusListener;
-
+	private FinderStatusListener statusListener;
+	private final String charset = "UTF-8";
+	
 	@Override
 	public FinderResult find(InputStream inputText, String searchString) {
 		FinderResult result = new FinderResult();
 		
-		String text = StreamsHelper.inputStreamToString(inputText);
-		
-		if(text.contains(searchString)) {
-			result.found = true;
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new InputStreamReader(inputText, charset));
+		} catch (UnsupportedEncodingException ex) {
+			reader = new BufferedReader(new InputStreamReader(inputText));
+			ex.printStackTrace();
 		}
+		
+		try {
+			int currentLinePosition = 1;
+			long bytesRead = 0;
+			String line = reader.readLine();
+			while(line != null) {
+				int column = line.indexOf(searchString);
+				
+				if(column != -1) {
+					Position hitPosition = new Position(currentLinePosition, column+1);
+					result.found = true;
+					result.hits.add(hitPosition);
+					result.numberOfHits++;
+					
+					if(statusListener != null) {
+						statusListener.searchStringFound(hitPosition);
+						bytesRead += line.getBytes(charset).length;
+						statusListener.progressUpdate(bytesRead);
+					}
+				}				
+				line = reader.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		return result;		
 	}
 		
