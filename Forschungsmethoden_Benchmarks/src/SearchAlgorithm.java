9017b5de92ae3b6665be6a7ff3e1d9c7a4556f86
fleckb <bernhard.fleck@gmail.com>
2012-11-19 15:48:51 +0100

    removed unused classes in default package

--- Forschungsmethoden_Benchmarks/src/SearchAlgorithm.java
+++
@@ -1,9 +0,0 @@
-import java.io.InputStream;
-import java.util.HashMap;
-
-public interface SearchAlgorithm {
-	
-	public Object doSearch(InputStream input, String searchstring, int num);
-	
-	public HashMap<String,String> getFinal();
-}
