8433a55eff37d38eaaa83013bc118d175cd9145f
fleckb <bernhard.fleck@gmail.com>
2012-11-25 19:49:55 +0100

    command line app

--- Forschungsmethoden_Benchmarks/src/benchmark/Tester.java
+++ Forschungsmethoden_Benchmarks/src/benchmark/Tester.java
@@ -1,25 +1,19 @@
 package benchmark;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.logging.Logger;
-import java.lang.management.*;
-
-import javax.swing.JPanel;
 
 import benchmark.algorithms.Finder;
 import benchmark.algorithms.KnuthMorrisPrattFinder;
 import benchmark.algorithms.NaiveFinder;
 import benchmark.algorithms.RabinKarpFinder;
+import benchmark.harness.BenchmarkResult;
 import benchmark.harness.Benchmarker;
 import benchmark.reporter.CsvReporter;
+import benchmark.reporter.DefaultReporter;
 import benchmark.reporter.Reporter;
 
 /***
@@ -29,88 +23,82 @@ import benchmark.reporter.Reporter;
  */
 public class Tester {
 	
-	Logger log;
-	Finder algo = null;
-	String algoName = "";
-	Benchmarker bench = null;
-	Reporter reporter = null;
+	private Tester() {}
 	
-	public Tester(){}
-	
-	public Tester(String al, InputStream in, String word){
-		algo = selectAlgorithm(al);
-		reporter = new CsvReporter(algoName);
-		bench = new Benchmarker(algo);
-		bench.prepare(in, word);
-	}
-
-	private void reInit(String al, InputStream in, String word){
-		algo = selectAlgorithm(al);
-		reporter = new CsvReporter(algoName);
-		bench = new Benchmarker(algo);
-		bench.prepare(in, word);
-	}
-	
-	private Finder selectAlgorithm(String s){
-		Finder f = null;
-		
-		if(s.equalsIgnoreCase("naive")){
-			f = new NaiveFinder();
-			algoName = "Naive String Search";
-			}
-		else if(s.equalsIgnoreCase("kmp")){
-			f = new KnuthMorrisPrattFinder();
-			algoName = "Knut-Morris-Pratt String Search";
-			}
-		else if(s.equalsIgnoreCase("rk")){
-			f = new RabinKarpFinder();
-			algoName = "Rabin-Karp String Search";
-			}
-		else if(s.equalsIgnoreCase("bm")){
-			f = new RabinKarpFinder();
-			algoName = "Boyer-Moore String Search";
-			}
-		else {
-			System.err.println("Algorithm can only be \"naive\", \"kmp\", \"rk\" or \"bm\".");
-			System.exit(1);
-		}
-		
-		return f;
-	}
-	
-	private void start() {
-		bench.run();
-	}
-
+	/**
+	 * benchmark.Tester algorithm path-to-input-file search-string iterations [output-filename]
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
-		File file = null;
-		InputStream in = null;
-		OutputStream out = null;
+		if(args.length < 4) {
+			printUsage();
+		}
 		
-		if((args.length != 4) ||
-				!(Integer.getInteger(args[3]) == null)){
-			System.err.println("Usage: Tester <Algorithm> <Path-to-input-file> <Search string> <#>");
-			System.exit(1);
+		int iterations = 0;
+		try {
+			iterations = Integer.parseInt(args[3]);
+		} catch(Exception ex) {
+			System.err.print(ex.getMessage());
+			System.exit(1);		
+		}
+		
+		Finder algorithm = selectAlgorithm(args[0]);
+		
+		if(algorithm==null) {
+			printUsage();
+		}
+		
+		Reporter reporter;
+		File inputFile = new File(args[1]);
+		String searchString = args[2];
+				
+		Benchmarker benchmarker = new Benchmarker(algorithm);
+		
+		List<BenchmarkResult> results = new ArrayList<BenchmarkResult>();
+		
+		// TODO
+		if(args.length == 5) {
+			reporter = new CsvReporter(args[4]);
+		} else {
+			reporter = new DefaultReporter();
 		}
 		
 		try {
-			file = new File(args[1]);
-			
-			if(!file.exists()){
-				System.err.println("Inputfile not found.");
-				System.exit(1);
-				}
-			
-			in = new FileInputStream(file);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			for(int i=0; i<iterations; i++) {
+				InputStream inputStream = new FileInputStream(inputFile);
+				benchmarker.prepare(inputStream, searchString);
+				results.add(benchmarker.run());
+				inputStream.close();
+			}
+			reporter.report(results.toArray(new BenchmarkResult[0]));
+		} catch(Exception ex) {
+			System.err.print(ex.getMessage());
 		}
-		
-		int iterations = Integer.parseInt(args[3]);
-		Tester tester = new Tester(args[0], in, args[2]);
-		tester.start();
-		
-		//TODO Implement Checks etc. for several runs, with new input
+		System.exit(0);
+	}
+	
+	private static void printUsage() {
+		System.err.println("Usage: benchmark.Tester algorithm path-to-input-file search-string iterations [output-basename]");
+		System.err.println("algorithm - one of:" + 
+				"\n   naive (Naive Search)" +
+				"\n   kmp (Knuth-Morris-Pratt" +
+				"\n   rk (Rabin-Karp" +
+				"\n   bm (Boyer-Moore\n");
+		System.exit(1);		
+	}
+
+	private static Finder selectAlgorithm(String finderString) {		
+		if(finderString.equalsIgnoreCase("naive")){
+			return new NaiveFinder();
+		} else if(finderString.equalsIgnoreCase("kmp")){
+			return new KnuthMorrisPrattFinder();
+		} else if(finderString.equalsIgnoreCase("rk")){
+			return new RabinKarpFinder();
+		} else if(finderString.equalsIgnoreCase("bm")){
+			return new RabinKarpFinder();
+		} 
+		return null;
 	}
 }
